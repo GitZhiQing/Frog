745c275b6ee9c05071092e710b1602834a77d911
QING <qlear@qq.com>
2025-04-08 02:03:41 +0800

    feat(config): 添加博客基本配置并优化数据结构

--- app/config.py
+++ app/config.py
@@ -9,17 +9,24 @@ from dotenv import load_dotenv
 class Config:
     """默认配置 - 开发环境"""
 
+    # Flask 应用
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     FLASK_ENV: Literal["development", "production"] = "production"
     DEBUG: bool = False
     FLASK_DEBUG: bool = False
 
+    # 数据
     DATA_DIR: Path = Path(__file__).parent.parent / "data"
     POST_DIR: Path = DATA_DIR / "posts"
     POST_IMGS_DIR: Path = DATA_DIR / "imgs"
     DATABASE_PATH: Path = DATA_DIR / "db" / "data.db"
     __prefix: str = "sqlite:///" if sys.platform.startswith("win") else "sqlite:////"
     SQLALCHEMY_DATABASE_URI: str = f"{__prefix}{DATABASE_PATH}?check_same_thread=False"
+
+    # 博客
+    BLOG_NAME: str = os.getenv("BLOG_NAME")
+    BLOG_INTRO: str = os.getenv("BLOG_INTRO")
+    BLOG_AVATAR: str = os.getenv("BLOG_AVATAR")
 
 
 class ProductionConfig(Config):
